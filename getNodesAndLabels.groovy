98bc142e39100fe3976da9732067037203b8be65
Ryan Blomquist <=>
2019-02-26 14:47:48 -0600

    Get labels even if node is offline.

--- getNodesAndLabels.groovy
+++ getNodesAndLabels.groovy
@@ -4,17 +4,17 @@ import hudson.model.Computer.ListPossibleNames
 // and by displaying a slightly more human-readable format.
 for (_slave in hudson.model.Hudson.instance.slaves) {
     println("Node: ${_slave.name}")
+    println("Labels: ${_slave.getLabelString()}")
 
     // test the node for up/down and, if up, pull the IP addresses
     def computer = _slave.computer
     if (computer.online) {
-        println("Labels: ${_slave.getLabelString()}")
         println("IP Addresses:\n----------")
         def ips = jenkins.model.Jenkins.instance.getNode(_slave.name).getChannel().call(new ListPossibleNames())
         remoteIps = ips.findAll { ip -> !(ip ==~ /^172\..*/)}
         remoteIps.each { ip -> println "${ip}"}
     } else {
-        println("Unable to retrieve device information")
+        println("Device offline - no IP information")
     }
     println("\n")
 }
