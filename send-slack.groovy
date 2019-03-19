fc142b52030dd4037ed415c8ea41b7025fe122d1
gtrudea <47367082+gtrudea@users.noreply.github.com>
2019-03-19 14:41:50 -0500

    Create send-slack.groovy

---
+++ send-slack.groovy
@@ -0,0 +1,37 @@
+@Library('jenkins-reference-framework@cloudbees-86-slack')
+//import com.mrll.javelin.model.slack.MessageType
+import com.mrll.javelin.alert.SlackAlert
+import com.mrll.javelin.alert.AlertFactory
+
+def slackAlert = AlertFactory.create(SlackAlert, [channel: params.room, customMessage: params.messagetext, messageType: params.messagetypes])
+
+pipeline {
+//
+
+    agent any
+    
+    stages{
+        stage('build') {
+            steps{
+                script{
+                    sh 'printenv'
+                }
+            }
+        }
+        stage('notify') {
+            steps{
+                script{
+                    withCredentials([string(credentialsId: 'new-mrll-svc-slack', variable: 'slack_token')]) {
+                    try {
+                        slackAlert.send(env, currentBuild)
+                    } 
+                    catch(all) {
+                        echo "Slack send failed!!!! ${messageType} : ${repoNotifyRooms} : ${message}"
+                        throw all
+                    }
+                    }
+                }
+            }
+        }
+    }
+}
